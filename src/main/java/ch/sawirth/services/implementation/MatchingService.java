1d609ef3265b19e623b6176f409e6daba4c28538
Sandro Wirth <s.wirth303@gmail.com>
2017-10-17 14:01:29 +0200

    find matching results for overloaded methods

--- src/main/java/ch/sawirth/services/implementation/MatchingService.java
+++ src/main/java/ch/sawirth/services/implementation/MatchingService.java
@@ -7,14 +7,23 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.PackageDeclaration;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.Parameter;
-
+import com.github.javaparser.ast.nodeTypes.NodeWithSimpleName;
+import com.google.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class MatchingService implements IMatchingService {
+    private final Logger logger;
+
+    @Inject
+    public MatchingService(Logger logger) {
+        this.logger = logger;
+    }
+
     @Override
     public ClassRepresentation findMatchingClassRepresentation(
             ClassOrInterfaceDeclaration classDeclaration,
@@ -29,15 +38,19 @@ public class MatchingService implements IMatchingService {
             return filtered.get(0);
         }
 
-        CompilationUnit parentNode = (CompilationUnit)classDeclaration.getParentNode().get();
-        PackageDeclaration packageDeclaration = parentNode.getPackageDeclaration().get();
+        try {
+            CompilationUnit parentNode = (CompilationUnit)classDeclaration.getParentNode().get();
+            PackageDeclaration packageDeclaration = parentNode.getPackageDeclaration().get();
 
-        filtered = filtered.stream()
-                .filter(cp -> cp.fullName.contains(packageDeclaration.getNameAsString()))
-                .collect(Collectors.toList());
+            filtered = filtered.stream()
+                    .filter(cp -> cp.fullName.contains(packageDeclaration.getNameAsString()))
+                    .collect(Collectors.toList());
 
-        if (filtered.size() == 1) {
-            return filtered.get(0);
+            if (filtered.size() == 1) {
+                return filtered.get(0);
+            }
+        } catch (Exception ex) {
+            logger.log(Level.SEVERE, ex.toString());
         }
 
         return null;
@@ -55,12 +68,42 @@ public class MatchingService implements IMatchingService {
             return filtered.get(0);
         }
 
-        //TODO filter by method parameter names
-        List<String> parameterNames = new ArrayList<>();
-        for (Parameter parameter : methodDeclaration.getParameters()) {
+        //Filter by number of arguments
+        filtered = filtered.stream()
+                .filter(m -> m.methodArguments.size() == methodDeclaration.getParameters().size())
+                .collect(Collectors.toList());
 
+        if (filtered.size() == 1) {
+            return filtered.get(0);
         }
 
+        //Filter by names of arguments
+        List<MethodRepresentation> filteredByParameterNames = new ArrayList<>();
+        for (MethodRepresentation methodRepresentation : filtered) {
+            if (hasParametersWithSameNames(methodRepresentation, methodDeclaration)) {
+                filteredByParameterNames.add(methodRepresentation);
+            }
+        }
+
+        if (filteredByParameterNames.size() == 1) {
+            return filteredByParameterNames.get(0);
+        }
+
+        //TODO overloaded methods where method has same names but different types (who the hell even writes such code ¯\_(ツ)_/¯)
         return null;
+    }
+
+    private boolean hasParametersWithSameNames(MethodRepresentation methodRepresentation, MethodDeclaration methodDeclaration) {
+        List<String> methodRepNames = methodRepresentation.methodArguments.stream()
+                .map(a -> a.name)
+                .collect(Collectors.toList());
+
+        List<String> methodDeclarationNames = methodDeclaration.getParameters().stream()
+                .map(NodeWithSimpleName::getNameAsString)
+                .collect(Collectors.toList());
+
+        methodRepNames.removeAll(methodDeclarationNames);
+
+        return methodRepNames.size() == 0;
     }
 }
