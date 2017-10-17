3d45dfdaa90e2110eb49f11f7c730cb120ce087c
Sandro Wirth <s.wirth303@gmail.com>
2017-10-17 14:00:29 +0200

    build jar & script for running the program

---
+++ run-documenter.ps1
@@ -0,0 +1,12 @@
+$java = 'C:\Program Files\Java\jdk1.8.0_20\bin\java'
+$jar = 'C:\Users\Sandro\Documents\GitHub\SideEffectsDocumenter\build\libs\SideEffects-documenter-all-1.0.jar'
+$main = 'main.Main'
+
+# Purano result path
+$puranoPath = 'C:\Users\Sandro\Documents\GitHub\joda-time\Purano-Result.json'
+
+# root of -java files
+$javaRoot = 'C:\Users\Sandro\Documents\GitHub\joda-time\src\main\java'
+
+
+& $java -jar $jar $javaRoot -p $puranoPath
