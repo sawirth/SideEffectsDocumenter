729a25c7961fa426d9c948d21038368d25221054
Sandro Wirth <s.wirth303@gmail.com>
2017-10-17 13:59:48 +0200

    FieldModifier info

---
+++ src/main/java/ch/sawirth/model/purano/FieldModifier.java
@@ -0,0 +1,29 @@
+package ch.sawirth.model.purano;
+
+import java.util.Set;
+
+public class FieldModifier {
+    public final String name;
+    public final String type;
+    public final String owner;
+    public final boolean hasDirectAccess;
+    public final Set<Integer> dependsOnParameterFromIndex;
+    public final Set<FieldDependency> fieldDependencies;
+    public final Set<FieldDependency> staticFieldDependencies;
+
+    public FieldModifier(String name,
+                         String type,
+                         String owner, boolean hasDirectAccess,
+                         Set<Integer> dependsOnParameterFromIndex,
+                         Set<FieldDependency> fieldDependencies,
+                         Set<FieldDependency> staticFieldDependencies)
+    {
+        this.name = name;
+        this.type = type;
+        this.owner = owner;
+        this.hasDirectAccess = hasDirectAccess;
+        this.dependsOnParameterFromIndex = dependsOnParameterFromIndex;
+        this.fieldDependencies = fieldDependencies;
+        this.staticFieldDependencies = staticFieldDependencies;
+    }
+}
--- src/main/java/ch/sawirth/model/purano/MethodRepresentation.java
+++ src/main/java/ch/sawirth/model/purano/MethodRepresentation.java
@@ -6,12 +6,15 @@ public class MethodRepresentation {
     public final String name;
     public final String purityType;
     public final List<MethodArgument> methodArguments;
+    public final List<FieldModifier> fieldModifiers;
 
     public MethodRepresentation(String name,
                                 String purityType,
-                                List<MethodArgument> methodArguments) {
+                                List<MethodArgument> methodArguments,
+                                List<FieldModifier> fieldModifiers) {
         this.name = name;
         this.purityType = purityType;
         this.methodArguments = methodArguments;
+        this.fieldModifiers = fieldModifiers;
     }
 }
